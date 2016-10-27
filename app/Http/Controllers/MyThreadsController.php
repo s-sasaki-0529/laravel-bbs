d46f14088f7090b0ce0504bf02c0d58a1598f84b
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-10-27 08:36:20 +0000

    新しいスレッドを投稿できるようにした

--- app/Http/Controllers/MyThreadsController.php
+++ app/Http/Controllers/MyThreadsController.php
@@ -35,6 +35,15 @@ class MyThreadsController extends Controller
 
     /* スレッドを新規作成 */
     public function createThread (Request $request) {
+      $data = $request->all();
+      $thread_data = array('title' => $data['title']);
+      $this->thread->fill($thread_data);
+      $this->thread->save();
+      $writing = new Writing;
+      $writing_data = array('thread_id' => $this->thread->id , 'name' => $data['name'] , 'body' => $data['body']);
+      $writing->fill($writing_data);
+      $writing->save();
+      return redirect()->to('/thread/detail/' . $this->thread->id);
     }
 
     /* スレッドに書き込みを投稿 */
--- resources/views/MyThreads/create.blade.php
+++ resources/views/MyThreads/create.blade.php
@@ -9,11 +9,11 @@
       </div>
       <div class='form-group'>
         <label class='control-label'>投稿者名</label>
-        <input class='form-control' type="text" name="title" id="title">
+        <input class='form-control' type="text" name="name" id="name">
       </div>
       <div class='form-group'>
         <label class='control-label'>本文</label>
-        <textarea class='size-fix form-control' name="title" id="title"></textarea>
+        <textarea class='size-fix form-control' name="body" id="body"></textarea>
       </div>
       <div class='right form-group'>
         <button type="submit" class='btn btn-default'>作成</button>
